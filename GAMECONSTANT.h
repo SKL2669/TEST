58654618abc4a73b546611062bb5f2dcea033197
SKL2669 <109145437+SKL2669@users.noreply.github.com>
2022-07-16 07:41:48 -0400

    Add files via upload

---
+++ GAMECONSTANT.h
@@ -0,0 +1,8 @@
+#ifndef GAMECONSTANT
+#define GAMECONSTANT
+
+const int STAGE_WIDTH[3] = { 10, 12, 11};
+const int STAGE_HEIGHT[3] = { 10, 12, 11};
+const int KEY_COUNT[3] = { 1, 3, 3};
+
+#endif GAMECONSTANT
